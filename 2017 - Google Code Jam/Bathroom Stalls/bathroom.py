bb32f9d37053056b1b2a5049b37ed83827632b6c
Maarti <bryan.martinet@gmail.com>
2017-12-05 23:53:05 +0100

    begin algo for small data

--- 2017 - Google Code Jam/Bathroom Stalls/bathroom.py
+++ 2017 - Google Code Jam/Bathroom Stalls/bathroom.py
@@ -7,9 +7,29 @@ for i in range(1, t + 1):
   stalls = [0]*(nbStall+2)
   stalls[0],stalls[nbStall+1] = 1,1
   
+  # Datastructure
   if nbPrsn >= nbStall:
     max,min=0,0
   else:
+    values = []
+    for index in range(1,len(stalls)-1):
+      if stalls[index] == 0:
+        
+        # Compute Ls
+        j = index
+        while stalls[j] == 0:
+          j-=1
+        Ls = index - j
+        
+        # Compute Ls
+        j = index
+        while stalls[j] == 0:
+          j+=1
+        Rs = j - index        
+
+        values.append({"index":index,"Ls":Ls,"Rs":Rs})
+        
+    print(values)
     max = 1
     min = 1
   print("Case #{}: {} {}".format(i, max,min))
