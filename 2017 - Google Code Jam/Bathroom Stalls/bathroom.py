08b3c560412ca76f3ece116fc762a1558260cf8b
Maarti <bryan.martinet@gmail.com>
2017-12-06 00:57:15 +0100

    reasoning change

--- 2017 - Google Code Jam/Bathroom Stalls/bathroom.py
+++ 2017 - Google Code Jam/Bathroom Stalls/bathroom.py
@@ -1,3 +1,5 @@
+import math
+
 t = int(input())
 for i in range(1, t + 1):
   # Init
@@ -11,25 +13,29 @@ for i in range(1, t + 1):
   if nbPrsn >= nbStall:
     max,min=0,0
   else:
-    values = []
-    for index in range(1,len(stalls)-1):
-      if stalls[index] == 0:
-        
-        # Compute Ls
-        j = index
-        while stalls[j] == 0:
-          j-=1
-        Ls = index - j
-        
-        # Compute Ls
-        j = index
-        while stalls[j] == 0:
-          j+=1
-        Rs = j - index        
+    for p in range(nbPrsn):
+      values = []
+      start,size=-1,0
+      for index in range(1,len(stalls)):
+        if stalls[index] == 0:
+          if start == -1:
+            start = index
+          size+=1
+        elif size>0:
+          values.append({"start":start,"size":size,"best":math.ceil(size/2+start-1)})
+          start,size=-1,0    
+      
+      # Choose the better
+      size = -1
+      for index in range(len(values)):
+        if values[index]["size"] > size:
+          size = values[index]["size"]
+          choice = values[index]["best"]
+      print("prsn:{} choice:{}".format(p,choice))
+      stalls[choice] = 1
 
-        values.append({"index":index,"Ls":Ls,"Rs":Rs})
-        
-    print(values)
     max = 1
     min = 1
+  print(stalls)
   print("Case #{}: {} {}".format(i, max,min))
+  print("")
